5a28eb4a664821508bea225a42e2286dbe67d9de
Christoph Lütjen <christoph.luetjen@40three.io>
2021-01-05 21:02:44 +0100

    cleanup units

--- data/units/units.ts
+++ data/units/units.ts
@@ -2,12 +2,27 @@ import { Unit } from '../../abstractions';
 
 /** Supported units with descriptions */
 export const u = {
+    // length (metric)
     'mm': Unit({ name: 'millimeter' }),
-    'cm': Unit({ name: 'Centimeter' }),
+    'cm': Unit({ name: 'centimeter' }),
     'm': Unit({ name: 'meter' }),
+
+    // length (imperial)
+    'in': Unit({ name: 'inch' }),
+    'ft': Unit({ name: 'foot' }),
+
+    // weight (metric)
+    'mg': Unit({ name: 'milligram' }),
     'gr': Unit({ name: 'gram' }),
+    'kg': Unit({ name: 'kilogram' }),
+
+    // volume (metric)
+    'ml': Unit({ name: 'milliliter' }),
+    'l': Unit({ name: 'liter' }),
+    'cbm': Unit({ name: 'cubic meter' }),
+
+    // misc
     'px': Unit({ name: 'pixel' }),
     'pt': Unit({ name: 'point', description: 'Point (typography)', reference: 'https://en.wikipedia.org/wiki/Point_(typography)' }),
-    'kg': Unit({ name: 'kilogram' }),
     'V': Unit({ name: 'volt' }),
 };
